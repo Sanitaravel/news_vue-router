31f429a965d01e5925a9494ceae7a895ca943577
Alexvue <58359823+Alexvue@users.noreply.github.com>
2020-01-12 12:11:36 +0300

    Create Card.vue

---
+++ src/components/Card.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-card
+        class="mx-auto"
+        max-width="400"
+        color="black"
+    >
+        <v-img
+            class="white--text align-end"
+            height="200px"
+            :src="url_kek"
+        >
+            <v-card-title>{{title}}</v-card-title>
+        </v-img>
+
+        <v-card-subtitle class="pb-0">{{author}}</v-card-subtitle>
+
+        <v-card-text class="white--text">
+            <div>{{descr}}</div>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-btn
+            color="orange"
+            text
+            >
+            Share
+            </v-btn>
+
+            <v-btn @click="explore()"
+            color="orange"
+            text
+            >
+            Explore
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: ['author', 'title', 'descr', 'url_kek', 'url'],
+    methods:{
+        explore(){
+            window.open(this.url)
+        }
+    }
+}
+</script>
